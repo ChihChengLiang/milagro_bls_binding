61bc4d7824438c7ade2f4f7ff49dc2a404c3d3b6
Chih Cheng Liang <chihchengliang@gmail.com>
2019-08-20 18:41:07 +0800

    sign done

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,27 @@ use pyo3::prelude::*;
 use pyo3::types::PyBytes;
 use pyo3::wrap_pyfunction;
 
-use milagro_bls::PublicKey;
-use milagro_bls::SecretKey;
+use milagro_bls::{PublicKey, SecretKey, Signature};
 
 #[pyfunction]
-fn privtopub(_py: Python, a: Py<PyBytes>) -> PyObject {
-    let bytes = a.to_object(_py);
-    let bytes = bytes.cast_as::<PyBytes>(_py).unwrap();
-    let bytes = bytes.as_bytes();
-    let sk = SecretKey::from_bytes(bytes).unwrap();
+fn privtopub(_py: Python, k: Py<PyBytes>) -> PyObject {
+    let k_obj = k.to_object(_py);
+    let k_bytes = k_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes();
+    let sk = SecretKey::from_bytes(k_bytes).unwrap();
     let pk = PublicKey::from_secret_key(&sk);
     let obj = PyBytes::new(_py, pk.as_bytes().as_ref());
+    return obj.to_object(_py);
+}
+
+#[pyfunction]
+fn sign(_py: Python, msg: Py<PyBytes>, k: Py<PyBytes>, domain: u64) -> PyObject {
+    let k_obj = k.to_object(_py);
+    let k_bytes = k_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes();
+    let msg_obj = msg.to_object(_py);
+    let msg_bytes = msg_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes();
+    let sk = SecretKey::from_bytes(k_bytes).unwrap();
+    let sig = Signature::new(msg_bytes, domain, &sk);
+    let obj = PyBytes::new(_py, sig.as_bytes().as_ref());
     return obj.to_object(_py);
 }
 
@@ -20,6 +30,6 @@ fn privtopub(_py: Python, a: Py<PyBytes>) -> PyObject {
 #[pymodule]
 fn milagro_bls_binding(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(privtopub))?;
-
+    m.add_wrapped(wrap_pyfunction!(sign))?;
     Ok(())
 }
