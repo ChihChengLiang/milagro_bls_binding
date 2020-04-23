a751021d58eb46b1b44a889812df749203d74865
Chih Cheng Liang <chihchengliang@gmail.com>
2020-04-23 20:11:57 +0800

    add py3.8 into multilinux release

--- build_or_release.sh
+++ build_or_release.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-# /opt/python/cp37-cp37m/bin/pyo3-pack build --manylinux $MANYLINUX_VERSION --release -i /opt/python/cp37-cp37m/bin/python -i /opt/python/cp36-cp36m/bin/python
-# ls -1 target/wheels/*.whl | xargs -I%  auditwheel show %
- /opt/python/cp37-cp37m/bin/pyo3-pack publish --username $PYPI_USERNAME --manylinux $MANYLINUX_VERSION -i /opt/python/cp37-cp37m/bin/python -i /opt/python/cp36-cp36m/bin/python
+/opt/python/cp37-cp37m/bin/maturin publish \
+    --username $PYPI_USERNAME \
+    --password $PYPI_PASSWORD \
+    --manylinux $MANYLINUX_VERSION \
+    -i /opt/python/cp36-cp36m/bin/python \
+    -i /opt/python/cp37-cp37m/bin/python \
+    -i /opt/python/cp38-cp38/bin/python
--- docker-build.sh
+++ docker-build.sh
@@ -1,2 +1,7 @@
 docker build -f manylinux1.dockerfile -t bls_manylinux1:latest .
-docker run -e MANYLINUX_VERSION="1"  -e PYPI_USERNAME="$PYPI_USERNAME" -e PYO3_PACK_PASSWORD="$PYO3_PACK_PASSWORD" -v $PWD:/tmp/src bls_manylinux1:latest
+docker run -it --rm \
+    -e MANYLINUX_VERSION="1"  \
+    -e PYPI_USERNAME="$PYPI_USERNAME" \
+    -e PYPI_PASSWORD="$PYPI_PASSWORD" \
+    -v $PWD:/tmp/src \
+    bls_manylinux1:latest
