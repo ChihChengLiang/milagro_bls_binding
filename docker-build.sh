1c963fbf05695eb573389e3733b254f3019864b1
Chih Cheng Liang <chihchengliang@gmail.com>
2020-04-23 20:25:11 +0800

    better publish flow

--- docker-build.sh
+++
@@ -1,7 +0,0 @@
-docker build -f manylinux1.dockerfile -t bls_manylinux1:latest .
-docker run -it --rm \
-    -e MANYLINUX_VERSION="1"  \
-    -e PYPI_USERNAME="$PYPI_USERNAME" \
-    -e PYPI_PASSWORD="$PYPI_PASSWORD" \
-    -v $PWD:/tmp/src \
-    bls_manylinux1:latest
