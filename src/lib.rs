cb350b5a1bfd40bfd7f381b697b198ed29dd7790
Chih Cheng Liang <chihchengliang@gmail.com>
2020-04-26 17:30:04 +0800

    rollback to old version

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use pyo3::exceptions::ValueError;
 use pyo3::prelude::*;
 use pyo3::types::{PyBytes, PyList};
 use pyo3::wrap_pyfunction;
@@ -6,67 +5,72 @@ use pyo3::wrap_pyfunction;
 use milagro_bls::{AggregatePublicKey, AggregateSignature, PublicKey, SecretKey, Signature};
 
 #[pyfunction]
-fn PrivToPub(_py: Python, SK: Py<PyBytes>) -> PyResult<PyObject> {
-    let sk_obj = SK.to_object(_py);
-    let sk_bytes = sk_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes();
-    let sk = match SecretKey::from_bytes(sk_bytes) {
-        Ok(_sk) => _sk,
-        Err(_) => return Err(PyErr::new::<ValueError, &str>("Invalid Secrete Key")),
-    };
+fn privtopub(_py: Python, k: Py<PyBytes>) -> PyObject {
+    let k_obj = k.to_object(_py);
+    let k_bytes = k_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes();
+    let sk = SecretKey::from_bytes(k_bytes).unwrap();
     let pk = PublicKey::from_secret_key(&sk);
     let obj = PyBytes::new(_py, pk.as_bytes().as_ref());
-    return Ok(obj.to_object(_py));
+    return obj.to_object(_py);
 }
 
 #[pyfunction]
-fn Sign(_py: Python, SK: Py<PyBytes>, message: Py<PyBytes>) -> PyResult<PyObject> {
-    let sk_obj = SK.to_object(_py);
-    let sk_bytes = sk_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes();
-    let msg_obj = message.to_object(_py);
+fn sign(_py: Python, message_hashes: Py<PyBytes>, k: Py<PyBytes>, domain: u64) -> PyObject {
+    let k_obj = k.to_object(_py);
+    let k_bytes = k_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes();
+    let msg_obj = message_hashes.to_object(_py);
     let msg_bytes = msg_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes();
-    let sk = match SecretKey::from_bytes(sk_bytes) {
-        Ok(_sk) => _sk,
-        Err(_) => return Err(PyErr::new::<ValueError, &str>("Invalid Secrete Key")),
-    };
-    let sig = Signature::new(msg_bytes, &sk);
+    let sk = SecretKey::from_bytes(k_bytes).unwrap();
+    let sig = Signature::new(msg_bytes, domain, &sk);
     let obj = PyBytes::new(_py, sig.as_bytes().as_ref());
-    return Ok(obj.to_object(_py));
+    return obj.to_object(_py);
 }
 
 #[pyfunction]
-fn Verify(_py: Python, PK: Py<PyBytes>, message: Py<PyBytes>, signature: Py<PyBytes>) -> bool {
-    let msg_obj = message.to_object(_py);
+fn verify(
+    _py: Python,
+    message_hashes: Py<PyBytes>,
+    pubkey: Py<PyBytes>,
+    signature: Py<PyBytes>,
+    domain: u64,
+) -> bool {
+    let msg_obj = message_hashes.to_object(_py);
     let msg_bytes = msg_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes();
-    let pubkey_obj = PK.to_object(_py);
+    let pubkey_obj = pubkey.to_object(_py);
     let pubkey_bytes = pubkey_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes();
     let pk = match PublicKey::from_bytes(pubkey_bytes) {
-        Ok(_pk) => _pk,
+        Ok(pk) => pk,
         Err(_) => return false,
     };
     let sig_obj = signature.to_object(_py);
     let sig_bytes = sig_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes();
     let sig = match Signature::from_bytes(sig_bytes) {
-        Ok(_sig) => _sig,
+        Ok(sig) => sig,
         Err(_) => return false,
     };
-    return sig.verify(msg_bytes, &pk);
+    return sig.verify(msg_bytes, domain, &pk);
 }
 
 #[pyfunction]
-fn Aggregate(_py: Python, signatures: &PyList) -> PyObject {
+fn aggregate_signatures(_py: Python, signatures: &PyList) -> PyObject {
     let mut agg_sig = AggregateSignature::new();
-    signatures.iter().for_each(|sig| {
-        let sig_obj = sig.to_object(_py);
-        let sig_bytes = sig_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes();
-        let sig = Signature::from_bytes(sig_bytes).unwrap();
-        agg_sig.add(&sig);
-    });
+    let sigs: Vec<Signature> = signatures
+        .iter()
+        .map(|sig| {
+            let sig_obj = sig.to_object(_py);
+            let sig_bytes = sig_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes();
+            return Signature::from_bytes(sig_bytes).unwrap();
+        })
+        .collect();
+    for sig in &sigs {
+        agg_sig.add(sig);
+    }
     let obj = PyBytes::new(_py, agg_sig.as_bytes().as_ref());
     return obj.to_object(_py);
 }
 
 #[pyfunction]
-fn _AggregatePKs(_py: Python, pubkeys: &PyList) -> PyObject {
+fn aggregate_pubkeys(_py: Python, pubkeys: &PyList) -> PyObject {
     let pks: Vec<PublicKey> = pubkeys
         .iter()
         .map(|pubkey| {
@@ -76,82 +80,55 @@ fn _AggregatePKs(_py: Python, pubkeys: &PyList) -> PyObject {
         })
         .collect();
     let pks_ref: Vec<&PublicKey> = pks.iter().collect();
-    let agg_pub = AggregatePublicKey::aggregate(&pks_ref);
+    let agg_pub = AggregatePublicKey::from_public_keys(&pks_ref);
     let obj = PyBytes::new(_py, agg_pub.as_bytes().as_ref());
     return obj.to_object(_py);
 }
 
 #[pyfunction]
-fn FastAggregateVerify(
+fn verify_multiple(
     _py: Python,
-    PKs: &PyList,
-    message: Py<PyBytes>,
+    pubkeys: &PyList,
+    message_hashes: &PyList,
     signature: Py<PyBytes>,
+    domain: u64,
 ) -> bool {
-    let msg_obj = message.to_object(_py);
-    let msg_bytes = msg_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes();
-
+    let mut msg: Vec<u8> = vec![];
+    for message in message_hashes {
+        let msg_obj = message.to_object(_py);
+        let msg_bytes = msg_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes();
+        msg.extend_from_slice(msg_bytes);
+    }
     let sig_obj = signature.to_object(_py);
     let sig_bytes = sig_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes();
     let agg_sig = match AggregateSignature::from_bytes(sig_bytes) {
-        Ok(_agg_sig) => _agg_sig,
+        Ok(agg_sig) => agg_sig,
         Err(_) => return false,
     };
-    let mut pks: Vec<PublicKey> = vec![];
+    let mut pks: Vec<AggregatePublicKey> = vec![];
 
-    for pk in PKs {
+    for pk in pubkeys {
         let pubkey_obj = pk.to_object(_py);
         let pubkey_bytes = pubkey_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes();
-        match PublicKey::from_bytes(pubkey_bytes) {
-            Ok(_pk) => pks.push(_pk),
+        let aggpk = match AggregatePublicKey::from_bytes(pubkey_bytes) {
+            Ok(agg_pk) => agg_pk,
             Err(_) => return false,
         };
+        pks.push(aggpk);
     }
-
-    let pks_ref: Vec<&PublicKey> = pks.iter().collect();
-
-    return agg_sig.fast_aggregate_verify(&msg_bytes, &pks_ref);
-}
-
-#[pyfunction]
-fn AggregateVerify(_py: Python, pairs: &PyList, signature: Py<PyBytes>) -> bool {
-    let sig_obj = signature.to_object(_py);
-    let sig_bytes = sig_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes();
-    let agg_sig = match AggregateSignature::from_bytes(sig_bytes) {
-        Ok(_agg_sig) => _agg_sig,
-        Err(_) => return false,
-    };
-
-    let mut pks: Vec<PublicKey> = vec![];
-    let mut msgs: Vec<Vec<u8>> = vec![];
-    pairs.iter().for_each(|pair| {
-        let pubkey_obj = pair.get_item(0).unwrap().to_object(_py);
-        let pubkey_bytes = pubkey_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes();
-        let pk = PublicKey::from_bytes(pubkey_bytes).unwrap();
-
-        let msg_obj = pair.get_item(1).unwrap().to_object(_py);
-        let msg_bytes = msg_obj.cast_as::<PyBytes>(_py).unwrap().as_bytes().to_vec();
-
-        msgs.push(msg_bytes);
-        pks.push(pk);
-    });
-
-    let msgs_refs: Vec<&[u8]> = msgs.iter().map(|x| x.as_slice()).collect();
-    let pks_ref: Vec<&PublicKey> = pks.iter().map(|x| x).collect();
-
-    return agg_sig.aggregate_verify(&msgs_refs, &pks_ref);
+    let pks_ref: Vec<&AggregatePublicKey> = pks.iter().collect();
+    return agg_sig.verify_multiple(&msg[..], domain, &pks_ref);
 }
 
 /// This module is a python module implemented in Rust.
 #[pymodule]
 fn milagro_bls_binding(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
-    m.add_wrapped(wrap_pyfunction!(PrivToPub))?;
-    m.add_wrapped(wrap_pyfunction!(Sign))?;
-    m.add_wrapped(wrap_pyfunction!(Verify))?;
-    m.add_wrapped(wrap_pyfunction!(Aggregate))?;
-    m.add_wrapped(wrap_pyfunction!(_AggregatePKs))?;
-    m.add_wrapped(wrap_pyfunction!(FastAggregateVerify))?;
-    m.add_wrapped(wrap_pyfunction!(AggregateVerify))?;
+    m.add_wrapped(wrap_pyfunction!(privtopub))?;
+    m.add_wrapped(wrap_pyfunction!(sign))?;
+    m.add_wrapped(wrap_pyfunction!(verify))?;
+    m.add_wrapped(wrap_pyfunction!(aggregate_signatures))?;
+    m.add_wrapped(wrap_pyfunction!(aggregate_pubkeys))?;
+    m.add_wrapped(wrap_pyfunction!(verify_multiple))?;
     Ok(())
 }
--- test.py
+++ test.py
@@ -3,45 +3,96 @@ import milagro_bls_binding as bls
 
 import pytest
 
-from py_ecc.optimized_bls12_381 import (
-    G1,
-    G2,
-    multiply,
+CURVE_ORDER = (
+    52435875175126190479447740508185965837690552500527637822603658699938581184513
 )
-from py_ecc.bls.g2_primatives import (
-    G1_to_pubkey,
-    G2_to_signature,
-)
+EMPTY_SIGNATURE = b"\x00" * 96
+EMPTY_PUBKEY = b"\x00" * 48
 
 
-def to_bytes(i):
-    return i.to_bytes(48, "big")
+def assert_pubkey(obj):
+    assert isinstance(obj, bytes) and len(obj) == 48
 
 
-def bytes_range(l):
-    return [to_bytes(i) for i in l]
+def assert_signature(obj):
+    assert isinstance(obj, bytes) and len(obj) == 96
 
+
+def test_sanity():
+    msg_0 = b"\x32" * 32
+    domain = 123
+
+    # Test: Verify the basic sign/verify process
+    privkey_0 = (5566).to_bytes(48, "little")
+    sig_0 = bls.sign(msg_0, privkey_0, domain)
+    assert_signature(sig_0)
+    pubkey_0 = bls.privtopub(privkey_0)
+    assert_pubkey(pubkey_0)
+    assert bls.verify(msg_0, pubkey_0, sig_0, domain)
+
+    privkey_1 = (5567).to_bytes(48, "little")
+    sig_1 = bls.sign(msg_0, privkey_1, domain)
+    pubkey_1 = bls.privtopub(privkey_1)
+    assert bls.verify(msg_0, pubkey_1, sig_1, domain)
+
+    # Test: Verify signatures are correctly aggregated
+    aggregated_signature = bls.aggregate_signatures([sig_0, sig_1])
+    assert_signature(aggregated_signature)
+
+    # Test: Verify pubkeys are correctly aggregated
+    aggregated_pubkey = bls.aggregate_pubkeys([pubkey_0, pubkey_1])
+    assert_pubkey(aggregated_pubkey)
+
+    # Test: Verify with `aggregated_signature` and `aggregated_pubkey`
+    assert bls.verify(msg_0, aggregated_pubkey, aggregated_signature, domain)
+
+    # Test: `verify_multiple`
+    msg_1 = b"\x22" * 32
+    privkey_2 = (55688).to_bytes(48, "little")
+    sig_2 = bls.sign(msg_1, privkey_2, domain)
+    assert_signature(sig_2)
+    pubkey_2 = bls.privtopub(privkey_2)
+    assert_pubkey(pubkey_2)
+    sig_1_2 = bls.aggregate_signatures([sig_1, sig_2])
+    assert bls.verify_multiple(
+        pubkeys=[pubkey_1, pubkey_2],
+        message_hashes=[msg_0, msg_1],
+        signature=sig_1_2,
+        domain=domain,
+    )
+
+
+def test_invalid_signature():
+    domain = 123
+    msg_0 = b"\x32" * 32
+    privkey = (5567).to_bytes(48, "little")
+    pubkey = bls.privtopub(privkey)
+    invalid_sig = b"\x12" * 96
+    assert not bls.verify(msg_0, pubkey, invalid_sig, domain=domain)
+    assert not bls.verify_multiple(
+        pubkeys=[pubkey, pubkey],
+        message_hashes=[msg_0, msg_0],
+        signature=invalid_sig,
+        domain=domain,
+    )
+
+def test_invalid_pubkey():
+    domain = 123
+    invalid_pubkey = b"\x56" * 48
+    msg = b"\x32" * 32
+    privkey = (5566).to_bytes(48, "little")
+    sig = bls.sign(msg, privkey, domain)
+
+    assert not bls.verify(msg, invalid_pubkey, sig, domain=domain)
+    assert not bls.verify_multiple(
+        pubkeys=[invalid_pubkey, invalid_pubkey],
+        message_hashes=[msg, msg],
+        signature=sig,
+        domain=domain,
+    )
 
 @pytest.mark.parametrize(
-    'SKs,messages,success',
-    [
-        (bytes_range(range(1, 10)), range(1, 10), True),
-        (bytes_range([1,2,3]), [1,2,3], True),
-        # Test duplicate messages fail
-        (bytes_range([1,2,3]), [42, 69, 42], False),
-    ]
-)
-def test_aggregate_verify(SKs, messages, success):
-    PKs = [bls.PrivToPub(SK) for SK in SKs]
-    messages = [msg.to_bytes(32, "big") for msg in messages]
-    signatures = [bls.Sign(SK, msg) for SK, msg in zip(SKs, messages)]
-    aggregate_signature = bls.Aggregate(signatures)
-    assert bls.AggregateVerify(
-        list(zip(PKs, messages)), aggregate_signature) == success
-
-
-@pytest.mark.parametrize(
-    'privkey_int',
+    "privkey_int",
     [
         (1),
         (5),
@@ -49,39 +100,72 @@ def test_aggregate_verify(SKs, messages, success):
         (735),
         (127409812145),
         (90768492698215092512159),
-        # (0),
-    ]
+        (CURVE_ORDER - 1),
+    ],
 )
-def test_sign_verify(privkey_int):
-    privkey = to_bytes(privkey_int)
-    msg = str(privkey).encode('utf-8')
-    pub = bls.PrivToPub(privkey)
-    sig = bls.Sign(privkey, msg)
-    assert bls.Verify(pub, msg, sig)
+def test_bls_core_succeed(privkey_int):
+    domain = 0
+    privkey = privkey_int.to_bytes(48, "little")
+    msg = str(privkey).encode("utf-8")
+    sig = bls.sign(msg, privkey, domain=domain)
+    pub = bls.privtopub(privkey)
+    assert_pubkey(pub)
+    assert bls.verify(msg, pub, sig, domain=domain)
 
 
 @pytest.mark.parametrize(
-    'signature_points,result_point',
+    "msg, privkeys_int",
     [
-        ([multiply(G1, 2), multiply(G1, 3)], multiply(G1, 2 + 3)),
-        ([multiply(G1, 42), multiply(G1, 69)], multiply(G1, 42 + 69)),
-    ]
+        (
+            b"\x12" * 32,
+            [1, 5, 124, 735, 127409812145, 90768492698215092512159, CURVE_ORDER - 1],
+        ),
+        (b"\x34" * 32, [42, 666, 1274099945, 4389392949595]),
+    ],
 )
-def test_aggregate_pks(signature_points, result_point):
-    signatures = [G1_to_pubkey(pt) for pt in signature_points]
-    result_signature = G1_to_pubkey(result_point)
-    assert bls._AggregatePKs(signatures) == result_signature
+def test_signature_aggregation(msg, privkeys_int):
+    domain = 0
+    privkeys = [k.to_bytes(48, "little") for k in privkeys_int]
+    sigs = [bls.sign(msg, k, domain=domain) for k in privkeys]
+    pubs = [bls.privtopub(k) for k in privkeys]
+    aggsig = bls.aggregate_signatures(sigs)
+    aggpub = bls.aggregate_pubkeys(pubs)
+    assert bls.verify(msg, aggpub, aggsig, domain=domain)
 
 
+@pytest.mark.parametrize("msg_1, msg_2", [(b"\x12" * 32, b"\x34" * 32)])
 @pytest.mark.parametrize(
-    'SKs,message',
+    "privkeys_1_int, privkeys_2_int",
     [
-        (bytes_range(range(1, 5)), b'11'*48),
-    ]
+        (tuple(range(1, 11)), tuple(range(1, 11))),
+        ((1, 2, 3), (4, 5, 6, 7)),
+        ((1, 2, 3), (2, 3, 4, 5)),
+        ((1, 2, 3), ()),
+        ((), (2, 3, 4, 5)),
+    ],
 )
-def test_fast_aggregate_verify(SKs, message):
-    PKs = [bls.PrivToPub(sk) for sk in SKs]
-    signatures = [bls.Sign(sk, message) for sk in SKs]
-    aggregate_signature = bls.Aggregate(signatures)
-    assert bls.FastAggregateVerify(
-        PKs, message, aggregate_signature)
+def test_multi_aggregation(msg_1, msg_2, privkeys_1_int, privkeys_2_int):
+    domain = 0
+
+    privkeys_1 = [k.to_bytes(48, "little") for k in privkeys_1_int]
+    privkeys_2 = [k.to_bytes(48, "little") for k in privkeys_2_int]
+
+    sigs_1 = [
+        bls.sign(msg_1, k, domain=domain) for k in privkeys_1
+    ]  # signatures to msg_1
+    pubs_1 = [bls.privtopub(k) for k in privkeys_1]
+    aggpub_1 = bls.aggregate_pubkeys(pubs_1)  # sig_1 to msg_1
+
+    sigs_2 = [
+        bls.sign(msg_2, k, domain=domain) for k in privkeys_2
+    ]  # signatures to msg_2
+    pubs_2 = [bls.privtopub(k) for k in privkeys_2]
+    aggpub_2 = bls.aggregate_pubkeys(pubs_2)  # sig_2 to msg_2
+
+    message_hashes = [msg_1, msg_2]
+    pubs = [aggpub_1, aggpub_2]
+    aggsig = bls.aggregate_signatures(sigs_1 + sigs_2)
+
+    assert bls.verify_multiple(
+        pubkeys=pubs, message_hashes=message_hashes, signature=aggsig, domain=domain
+    )
