eb3faebb015908e08904f5a25247c77bf1b1a781
Chih Cheng Liang <chihchengliang@gmail.com>
2020-04-27 00:44:08 +0800

    remove old build scripts

--- build_or_release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-/opt/python/cp37-cp37m/bin/maturin publish \
-    --username $PYPI_USERNAME \
-    --password $PYPI_PASSWORD \
-    --manylinux $MANYLINUX_VERSION \
-    -i /opt/python/cp36-cp36m/bin/python \
-    -i /opt/python/cp37-cp37m/bin/python \
-    -i /opt/python/cp38-cp38/bin/python
