39e97c59da3b2d066d5ff848662f8649607ab852
Chih Cheng Liang <chihchengliang@gmail.com>
2020-08-25 17:57:07 +0800

    add pairing and test

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,63 @@ use pyo3::types::{PyBytes, PyList};
 use pyo3::wrap_pyfunction;
 
 use milagro_bls::{AggregatePublicKey, AggregateSignature, PublicKey, SecretKey, Signature};
+extern crate amcl;
+
+use self::amcl::bls381 as BLSCurve;
+
+pub use amcl::errors::AmclError;
+
+pub use BLSCurve::bls381::proof_of_possession::{G1_BYTES, G2_BYTES, SECRET_KEY_BYTES};
+use BLSCurve::ecp::ECP;
+use BLSCurve::ecp2::ECP2;
+
+pub use BLSCurve::bls381::utils::{
+    self, deserialize_g1, deserialize_g2, serialize_g1, serialize_g2, subgroup_check_g1,
+    subgroup_check_g2,
+};
+pub use BLSCurve::pair;
+
+pub type GroupG1 = ECP;
+pub type GroupG2 = ECP2;
+
+// Take a 384 bit array and convert to GroupG1 point (x, y)
+// See https://github.com/zkcrypto/pairing/blob/master/src/bls12_381/README.md#serialization
+pub fn decompress_g1(g1_bytes: &[u8]) -> Result<GroupG1, AmclError> {
+    // Ensure it is compressed
+    if g1_bytes.len() != G1_BYTES {
+        return Err(AmclError::InvalidG1Size);
+    }
+    deserialize_g1(g1_bytes)
+}
+
+// Take a 384*2 bit array and convert to GroupG2 point (x, y)
+// See https://github.com/zkcrypto/pairing/blob/master/src/bls12_381/README.md#serialization
+pub fn decompress_g2(g2_bytes: &[u8]) -> Result<GroupG2, AmclError> {
+    // Ensure it is compressed
+    if g2_bytes.len() != G2_BYTES {
+        return Err(AmclError::InvalidG2Size);
+    }
+    deserialize_g2(g2_bytes)
+}
+
+#[pyfunction]
+fn pairing(_py: Python, g2py: &PyBytes, g1py: &PyBytes, final_exp: bool) -> PyResult<String> {
+    let g1_bytes: Vec<u8> = g1py.extract()?;
+    let g2_bytes: Vec<u8> = g2py.extract()?;
+    let mut g1 = decompress_g1(&g1_bytes)
+        .map_err(|e| PyErr::new::<ValueError, _>(format!("Bad G1: {:?}", e)))?;
+    let mut g2 = decompress_g2(&g2_bytes)
+        .map_err(|e| PyErr::new::<ValueError, _>(format!("Bad G2: {:?}", e)))?;
+    g1.affine();
+    g2.affine();
+    let mut pairing = pair::initmp();
+    pair::another(&mut pairing, &g2, &g1);
+    let mut v = pair::miller(&pairing);
+    if final_exp {
+        v = pair::fexp(&v);
+    }
+    Ok(v.tostring())
+}
 
 #[pyfunction]
 fn SkToPk(_py: Python, SK: &PyBytes) -> PyResult<PyObject> {
@@ -78,12 +135,7 @@ fn _AggregatePKs(_py: Python, PKs: &PyList) -> PyResult<PyObject> {
 }
 
 #[pyfunction]
-fn FastAggregateVerify(
-    _py: Python,
-    PKs: &PyList,
-    message: &PyBytes,
-    signature: &PyBytes,
-) -> bool {
+fn FastAggregateVerify(_py: Python, PKs: &PyList, message: &PyBytes, signature: &PyBytes) -> bool {
     let msg_bytes: Vec<u8> = match message.extract() {
         Ok(bytes) => bytes,
         Err(_) => return false,
@@ -99,7 +151,7 @@ fn FastAggregateVerify(
     let mut pks: Vec<PublicKey> = vec![];
 
     for pk in PKs {
-        let pubkey_bytes = match pk.extract(){
+        let pubkey_bytes = match pk.extract() {
             Ok(bytes) => bytes,
             Err(_) => return false,
         };
@@ -127,7 +179,7 @@ fn AggregateVerify(_py: Python, PKs: &PyList, messages: &PyList, signature: &PyB
 
     let mut pks: Vec<PublicKey> = vec![];
     for pk in PKs {
-        let pubkey_bytes = match pk.extract(){
+        let pubkey_bytes = match pk.extract() {
             Ok(bytes) => bytes,
             Err(_) => return false,
         };
@@ -155,6 +207,7 @@ fn AggregateVerify(_py: Python, PKs: &PyList, messages: &PyList, signature: &PyB
 #[pymodule]
 fn milagro_bls_binding(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
+    m.add_wrapped(wrap_pyfunction!(pairing))?;
     m.add_wrapped(wrap_pyfunction!(SkToPk))?;
     m.add_wrapped(wrap_pyfunction!(Sign))?;
     m.add_wrapped(wrap_pyfunction!(Verify))?;
