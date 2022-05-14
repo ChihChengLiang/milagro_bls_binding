ce1330467a85fb40eb7131b0df322818abc90705
github-action-benchmark <github@users.noreply.github.com>
2022-05-14 00:11:04 +0000

    add Benchmark (pytest) benchmark result for d7d4cf3bf2ef46b2100bcafb571d503da4c4d39b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1652484865809,
+  "lastUpdate": 1652487064292,
   "repoUrl": "https://github.com/ChihChengLiang/milagro_bls_binding",
   "entries": {
     "Benchmark": [
@@ -2793,6 +2793,51 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000005379653675349857",
             "extra": "mean: 6.560244660999785 msec\nrounds: 100"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "chihchengliang@gmail.com",
+            "name": "Chih Cheng Liang",
+            "username": "ChihChengLiang"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d7d4cf3bf2ef46b2100bcafb571d503da4c4d39b",
+          "message": "Merge pull request #38 from ChihChengLiang/bump-upstrea\n\nBump upstream and cleanup error handling",
+          "timestamp": "2022-05-14T01:29:42+02:00",
+          "tree_id": "b1c31df099e8896750ab1ceb6966028ce0378a23",
+          "url": "https://github.com/ChihChengLiang/milagro_bls_binding/commit/d7d4cf3bf2ef46b2100bcafb571d503da4c4d39b"
+        },
+        "date": 1652487063275,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmark.py::test_verifying_aggregate_of_128_signatures",
+            "value": 20.083602482008217,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0006568968469517694",
+            "extra": "mean: 49.79186382999984 msec\nrounds: 10"
+          },
+          {
+            "name": "benchmark.py::test_verifying_aggregation_of_128_signatures_with_two_distinct_messages",
+            "value": 18.353513212292675,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000888951141156032",
+            "extra": "mean: 54.48548125000002 msec\nrounds: 10"
+          },
+          {
+            "name": "benchmark.py::test_single_verify",
+            "value": 136.31529496971777,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00035505938783794667",
+            "extra": "mean: 7.335933948000104 msec\nrounds: 100"
           }
         ]
       }
